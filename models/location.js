4c5c725d76f07a7abe1bf6290b92397b8709cd0f
PN-Barnes <paulieb99@gmail.com>
2021-07-29 11:37:12 -0700

    completed model for location.js

--- models/location.js
+++ models/location.js
@@ -4,6 +4,18 @@ const sequelize = require('../config/connection');
 class Location extends Model {} 
 Location.init(
     {
-        
+        id: {
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len:[1,30],
+                isAlpha: true
+            }
+        }
     }
 )
